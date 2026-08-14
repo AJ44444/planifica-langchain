5eb3a122356fad36c7d389112993af02c2e2ed3e
Alejandro Us <umalejandro30@gmail.com>
2026-08-14 16:58:50 -0600

    update: Update system prompt

--- app/prompts/system_prompts.py
+++ app/prompts/system_prompts.py
@@ -7,98 +7,27 @@ Extrae la estructura de un currículo con formato del Currículum Nacional Base 
 La estructura se compone de áreas curriculares y subáreas curriculares que pertenecen a cada área curricular.
 Un área curricular se compone por nombre del área, competencias del área, actividades sugeridas y criterios de evaluación.
 Una subárea curricular se compone por nombre de la subárea, competencias, indicadores y contenidos.
-Salida por cada área curricular:
-nombre_carrera
-nombre_area
-competencias_area:
-    string
-actividades_sugeridas:
-    string
-criterios_evaluacion_sugeridos:
-    string
-subareas:
-nombre_subarea
-competencias:
-    id_competencia
-    descripcion
-    indicadores_logro:
-        id_indicador
-        descripcion
-        contenidos:
-            id_contenido
-            descripcion
 Los identificadores deben ser los numeros al lado del texto, ejemplo: id_competencia: '1', id_indicador: '1.1', id_contenido: '1.1.1'.
 """
 
 SYSTEM_PROMPT_SCHOOL_LESSON_PLANS = """
 Busca la carrera que necesita el usuario, las áreas que pertenecen a la carrera y la subárea que necesita el usuario.
-Solicita el árbol curricular de la subárea seleccionada, recibe la respuesta y construye la planificación utilizando únicamente el árbol curricular, guarda la planificación en el sistema.
-Las actividades de aprendizaje deben estar alineadas a los contenidos, se redactan de forma impersonal, utilizando verbos en infinitivo: 'Presentar...', 'Analizar...', 'Desarrollar...'.
-Si la duración de la planificación es 1 (Un día) las actividades se estructuran en 'inicio', 'desarrollo' y 'cierre'.
-Si la duración es 2 (Una semana) las actividades se estructuran de forma secuencial. 
-Si la duración es 3 (Un bimestre - 8 semanas) las actividades se estructuran en proyectos y actividades bimestrales.
-Árbol curricular:
-    id_competencia
-    competencia
-    indicadores:
-        id_indicador
-        indicador
-        contenidos:
-            id_contenido
-            descripcion
-Salida para una planificación:
-metadatos:
-    nombre_carrera
-    subarea_curricular
-encabezado:
-    centro_educativo
-    lugar
-    grado
-    seccion
-    nombre_docente
-    duracion
-desarrollo_curricular:
-    id_fila
-    competencia
-    indicadores_logro:
-        indicador
-        contenidos:
-            string
-    actividades_aprendizaje:
-        id_actividad
-        fase
-        descripcion
+Solicita el árbol curricular de la subárea seleccionada, ingresa tal cual competencias, indicadores y contenidos en la planificación.
+Redacta las actividades de aprendizaje, deben estar alineadas a los contenidos, se redactan de forma impersonal, utilizando verbos en infinitivo: 'Presentar...', 'Analizar...', 'Desarrollar...'.
+Si la duración es de un día, las actividades se estructuran en 'inicio', 'desarrollo' y 'cierre'.
+Si la duración es de una semana, las actividades se estructuran de forma secuencial.
+Si la duración es de ocho semanas (Un bimestre), las actividades se estructuran en proyectos y actividades bimestrales.
 """
 
 
 SYSTEM_PROMPT_SCHOOL_ASSESSMENT_INSTRUMENTS = """
 Elabora el instrumento de evaluación considerando la complejidad pedagógica y la fase de la actividad evaluada.
 Los tipos de instrumentos de evaluación válidos son: 'lista_cotejo', 'rubrica' o 'escala_rango'.
-Salida para un instrumento de evaluación:
-id_planificacion
-id_fila
-id_actividad
-tipo
-titulo
-instrumento_generado:
-    escala:
-        string
-    criterios:
-        nombre
-        definiciones:
-            string
 """
 
 
 SYSTEM_PROMPT_SCHOOL_MULTIMODAL_RESOURCES = """
 Sugiere un recurso didáctico para la actividad de aprendizaje considerando la fase y el tipo de recurso más adecuado.
-Salida para un recurso didáctico:
-id_planificacion
-id_fila
-id_actividad
-tipo
-titulo
-url
 """
 
 
